e6f4575664146c1bfd0f53ef15327b022bb2e872
k0maru3 <k017maru@gmail.com>
2024-10-24 23:23:39 +0800

    头部注释新增修改历史

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -7,17 +7,25 @@
  * 
  * @copyright Copyright (c) 2023
  * 
+ * @par 修改日志
+ *  <Date>     | <Version> | <Author>       | <Description>                   *
+ *----------------------------------------------------------------------------*
+ *  2024/10/24 | 0.0.0.2   | K0maru         | 整理代码注释规范                 *      
  */
 #include "Arduino.h"
 #include "ChappieUI/ChappieUI.h"
 
 CHAPPIEUI ChappieUI;
-
+/**
+ * @brief 系统初始化并启动App_Launcher 
+ */
 void setup()
 {
     ChappieUI.begin();
 }
-
+/**
+ * @brief 刷新App_Launcher以获得实时信息
+ */
 void loop()
 {
     ChappieUI.update();
